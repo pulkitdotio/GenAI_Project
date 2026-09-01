e9340097494a32b244faef8f987ba6539033596f
Pulkit Sharma <divyanshu3110@gmail.com>
2026-09-02 02:24:37 +0530

    Implemented mongoDB connection

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+}
