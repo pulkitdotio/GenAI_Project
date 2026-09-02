20c56f8678239bde8d058843223abbd8c6768fed
Pulkit Sharma <divyanshu3110@gmail.com>
2026-09-03 02:43:31 +0530

    checking token blacklisting in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
+const blacklistModel = require('../models/blacklist.model');
 
 
-function authUser(req, res, next) {
+async function authUser(req, res, next) {
     const token = req.cookies.token;
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const isBlacklisted = await blacklistModel.findOne({ token });
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Token is invalid' });
     }
 
     try {
